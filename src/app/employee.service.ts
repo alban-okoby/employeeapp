8e364fda25dcd75fba6b8ddd71276856555e9fd9
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2022-09-22 18:10:24 +0200

    Add employeee service

---
+++ src/app/employee.service.ts
@@ -0,0 +1,30 @@
+import { Employee } from "./employee";
+import {Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+})
+export class EmployeeService {
+
+    private apiServeUrl = '';
+
+    constructor(private http: HttpClient) { }
+
+    public getEmployee(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServeUrl}/employee/all`);
+
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServeUrl}/employee/add, employee`);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {   
+        return this.http.put<Employee>(`${this.apiServeUrl}/employee/update, employee`);
+    }
+
+    public removeEmployee(employeeId: number): Observable<void> {   
+        return this.http.delete<void>(`${this.apiServeUrl}/employee/remove/${employeeId}`); 
+    }
+}
