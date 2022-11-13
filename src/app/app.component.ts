dd39dc49691e9255ed9e28427fdac7c98cf8771e
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2022-11-13 19:31:47 +0000

    Add edit delete and search methods feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
-
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +12,8 @@ export class AppComponent {
   title = 'employeeapp';
 
   public employees!: Employee[];
+  public editEmployee!: Employee;
+  public deleteEmployee!: Employee;
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -27,11 +29,40 @@ export class AppComponent {
     );
   }
 
-  public onOpenMoal(employee: Employee, mode: string): void {
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('addEmployeeForm')!.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      }
+    );
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.removeEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      }
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const mainContainer = document.getElementById('main-container')
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-bs-toggle', 'modal');
 
     if (mode === 'add')
       button.setAttribute('data-bs-target', '#addModal');
@@ -39,6 +70,25 @@ export class AppComponent {
       button.setAttribute('data-target', '#editModal');
     if (mode === 'delete')
       button.setAttribute('data-bs-target', '#deleteModal');
+
+    mainContainer!.appendChild(button);
+    button.click();
   }
 
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
 }
